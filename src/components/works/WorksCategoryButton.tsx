d3a139137289a5efb1ff8ed4a4cc3b4b877efb86
minzziPark <minzzi0068@gmail.com>
2024-08-27 18:00:58 +0900

    design : 카테고리 애니메이션 완료

--- src/components/works/WorksCategoryButton.tsx
+++ src/components/works/WorksCategoryButton.tsx
@@ -9,15 +9,19 @@ const WorksCategoryButton = ({ children, color, imgSrc, imgSize }: Props) => {
   return (
     <button
       className={
-        "w-[310px] h-[48px] flex items-center justify-center border-t " + color
+        "w-[310px] h-[48px] relative flex items-center justify-center border-t overflow-hidden duration-300 transition-all group"
       }
     >
-      <img
-        className={"block focus:hidden " + imgSize}
-        src={imgSrc}
-        alt="category_icon"
-      />
-      <p className="hidden focus:block">{children}</p>
+      <div
+        className={`absolute w-[310px] h-[48px] flex justify-center items-center font-Pretendard_Bold text-[15px] duration-300 transition-all transform -translate-y-full opacity-0 group-hover:translate-y-0 group-hover:opacity-100 bg-primary-white ${color}`}
+      >
+        <p>{children}</p>
+      </div>
+      <div
+        className={`absolute w-[310px] h-[48px] flex justify-center items-center duration-300 transition-all transform translate-y-0 opacity-100 group-hover:translate-y-full group-hover:opacity-0`}
+      >
+        <img src={imgSrc} alt="category_icon" className={`${imgSize}`} />
+      </div>
     </button>
   );
 };
