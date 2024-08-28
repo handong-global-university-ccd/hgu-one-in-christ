7edd94b86d928b9d770ecf05508c3392ca7f12cc
minzziPark <minzzi0068@gmail.com>
2024-08-28 15:02:08 +0900

    design : works 카테고리 애니메이션 디테일 수정

--- src/components/works/WorksCategoryButton.tsx
+++ src/components/works/WorksCategoryButton.tsx
@@ -13,12 +13,18 @@ const WorksCategoryButton = ({ children, color, imgSrc, imgSize }: Props) => {
       }
     >
       <div
-        className={`absolute w-[310px] h-[48px] flex justify-center items-center font-Pretendard_Bold text-[15px] duration-300 transition-all transform -translate-y-full opacity-0 group-hover:translate-y-0 group-hover:opacity-100 bg-primary-white ${color}`}
+        className={`absolute w-[310px] h-[48px] flex justify-center items-center font-Organetto_ExtBold text-[15px] duration-300 transition-all transform opacity-0 group-hover:opacity-100 bg-primary-white ${color}
+				group-focus:opacity-100 bg-primary-white ${color}`}
       >
-        <p>{children}</p>
+        <p
+          className={`duration-500 transition-all transform -translate-y-full opacity-0 group-hover:translate-y-0 
+					group-focus:translate-y-0 group-hover:opacity-100 group-focus:opacity-100`}
+        >
+          {children}
+        </p>
       </div>
       <div
-        className={`absolute w-[310px] h-[48px] flex justify-center items-center duration-300 transition-all transform translate-y-0 opacity-100 group-hover:translate-y-full group-hover:opacity-0`}
+        className={`absolute w-[310px] h-[48px] flex justify-center items-center duration-300 transition-all transform translate-y-0 opacity-100 group-hover:translate-y-full group-hover:opacity-0 group-focus:translate-y-full group-focus:opacity-0`}
       >
         <img src={imgSrc} alt="category_icon" className={`${imgSize}`} />
       </div>
