fef5c582a933ba823819b3717d3315c2f8c9ec0d
minzziPark <minzzi0068@gmail.com>
2024-08-31 21:35:28 +0900

    design : 글꼴 수정
    피그마상 medium 글꼴로 되어있어서 medium으로 설정했다가
    결과가 오히려 light와 일치하는 것 같아서 light로 일단 변경하였음

--- src/components/works/WorkList.tsx
+++ src/components/works/WorkList.tsx
@@ -34,21 +34,28 @@ const WorkList = ({ category }: Props) => {
               }`}
             >
               <div
-                className={`w-[201px] h-[24px] hidden group-hover:block ${
-                  category === "COMMUNICATION"
-                    ? "bg-primary-orange"
-                    : category === "SERVICE"
-                    ? "bg-primary-red"
-                    : category === "UXUI"
-                    ? "bg-primary-purple"
-                    : "bg-primary-blue"
-                }`}
+                className={`hidden w-[201px] h-[24px] group-hover:flex justify-center items-center 
+									${
+                    category === "COMMUNICATION"
+                      ? "bg-primary-orange"
+                      : category === "SERVICE"
+                      ? "bg-primary-red"
+                      : category === "UXUI"
+                      ? "bg-primary-purple"
+                      : "bg-primary-blue"
+                  }`}
               >
-                <p className={`text-primary-white text-center`}>예시문구</p>
+                <p
+                  className={`text-primary-white text-center font-Pretendard_Bold text-[12px]`}
+                >
+                  예시문구
+                </p>
               </div>
             </div>
-            <div className={`w-[288px] mb-[29px]`}>
-              <p className="text-primary-white ">{work.title}</p>
+            <div
+              className={`w-[288px] mb-[29px] font-TT_Firs_Light text-[15px]`}
+            >
+              <p className="text-primary-white">{work.title}</p>
               <div className="flex">
                 {work.designer.map((item, index, designer) => (
                   <React.Fragment key={index}>
