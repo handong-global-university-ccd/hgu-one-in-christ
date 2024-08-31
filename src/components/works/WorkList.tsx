b002e811c307c5deb86f6fe54f6bdc9d59b7e64c
minzziPark <minzzi0068@gmail.com>
2024-08-31 17:43:38 +0900

    design : 그리드 작품 호버시 효과 적용

--- src/components/works/WorkList.tsx
+++ src/components/works/WorkList.tsx
@@ -14,12 +14,39 @@ const WorkList = ({ category }: Props) => {
     <div className="w-full grid grid-cols-4 gap-0 mt-[42px]">
       {works.map((work) => (
         <div
-          className={`m-[0.5px] outline outline-1 outline-solid outline-primary-white
+          className={`m-[0.5px] outline outline-1 outline-solid outline-primary-white group
 					`}
           key={work.id}
         >
           <div className="flex flex-col items-center">
-            <div className={`w-[288px] h-[216px] my-[10px] bg-gray-600`}></div>
+            <div
+              className={`w-[288px] h-[216px] 
+							flex justify-center items-center
+							my-[10px] bg-gray-600
+							group-hover:border ${
+                category === "COMMUNICATION"
+                  ? "border-primary-orange"
+                  : category === "SERVICE"
+                  ? "border-primary-red"
+                  : category === "UXUI"
+                  ? "border-primary-purple"
+                  : "border-primary-blue"
+              }`}
+            >
+              <div
+                className={`w-[201px] h-[24px] hidden group-hover:block ${
+                  category === "COMMUNICATION"
+                    ? "bg-primary-orange"
+                    : category === "SERVICE"
+                    ? "bg-primary-red"
+                    : category === "UXUI"
+                    ? "bg-primary-purple"
+                    : "bg-primary-blue"
+                }`}
+              >
+                <p className={`text-primary-white text-center`}>예시문구</p>
+              </div>
+            </div>
             <div className={`w-[288px] mb-[29px]`}>
               <p className="text-primary-white ">{work.title}</p>
               <div className="flex">
