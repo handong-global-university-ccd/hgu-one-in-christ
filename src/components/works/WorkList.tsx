bbecb82880400d69a55897b41d791410230eedf4
minzziPark <minzzi0068@gmail.com>
2024-08-31 15:47:02 +0900

    design : works 페이지 그리드 ui 구현

--- src/components/works/WorkList.tsx
+++ src/components/works/WorkList.tsx
@@ -10,10 +10,18 @@ const WorkList = ({ category }: Props) => {
   const works: Work[] = WORKS[category] || [];
 
   return (
-    <div>
+    <div className="w-full grid grid-cols-4 gap-0">
       {works.map((item) => (
-        <div className="text-white" key={item.id}>
-          {item.id}
+        <div
+          className={`flex flex-col m-[0.5px] outline outline-1 outline-solid outline-primary-white
+					`}
+          key={item.id}
+        >
+          <div className={`w-[288px] h-[216px]`}></div>
+          <p className="text-primary-white ">{item.title}</p>
+          {item.designer.map((item) => (
+            <p className="text-primary-white ">{item.name}</p>
+          ))}
         </div>
       ))}
     </div>
