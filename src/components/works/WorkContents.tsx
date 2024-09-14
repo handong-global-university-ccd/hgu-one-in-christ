34f061964dbe00c546d3af7347b90171910c8d26
minzziPark <minzzi0068@gmail.com>
2024-09-15 02:19:54 +0900

    design : 작품 상세 페이지 UI 구현

---
+++ src/components/works/WorkContents.tsx
@@ -0,0 +1,40 @@
+import { WorkCategory } from "../../models/category.model";
+import { Work } from "../../models/work.model";
+
+interface Props {
+  category: WorkCategory;
+  work: Work | null;
+}
+
+const WorkContents = ({ category, work }: Props) => {
+  return (
+    <div className={`flex flex-col text-primary-white`}>
+      <p
+        className={`font-Menda_Medium text-[22px] mb-[40px] ${
+          category === "COMMUNICATION"
+            ? "text-primary-orange"
+            : category === "SERVICE"
+              ? "text-primary-red"
+              : category === "UXUI"
+                ? "text-primary-purple"
+                : "text-primary-blue"
+        }`}
+      >{`${category} DESIGN`}</p>
+      <p className={`font-Pretendard_Bold text-[33px] mb-[14px]`}>
+        {work?.title}
+      </p>
+      <p className={`w-[687px] font-Pretendard_Regular text-[17px]`}>
+        작품을 소개하는 글을 적어주세요. 작품을 소개하는 글을 적어주세요. 작품을
+        소개하는 글을 적어주세요. 작품을 소개하는 글을 적어주세요. 작품을
+        소개하는 글을 적어주세요. 작품을 소개하는 글을 적어주세요. 작품을
+        소개하는 글을 적어주세요. 작품을 소개하는 글을 적어주세요. 작품을
+        소개하는 글을 적어주세요. 작품을 소개하는 글을 적어주세요. 작품을
+        소개하는 글을 적어주세요. 작품을 소개하는 글을 적어주세요. 작품을
+        소개하는 글을 적어주세요. 작품을 소개하는 글을 적어주세요. 작품을
+        소개하는 글을 적어주세요. (공백포함 284글자)
+      </p>
+    </div>
+  );
+};
+
+export default WorkContents;
