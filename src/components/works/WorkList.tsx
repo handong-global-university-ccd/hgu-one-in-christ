761fcd809613f35b20c17caf4dacbbad928b5c20
minzziPark <minzzi0068@gmail.com>
2024-09-22 04:07:42 +0900

    design : 그리드 반응형 구현

--- src/components/works/WorkList.tsx
+++ src/components/works/WorkList.tsx
@@ -4,6 +4,7 @@ import { WorkCategory } from "../../models/category.model";
 import { Work } from "../../models/work.model";
 import { Link } from "react-router-dom";
 import { PATHS } from "../../constants/paths";
+import gridtest1 from "../../assets/works/project/test/gridtest1.png";
 
 interface Props {
   category: WorkCategory;
@@ -13,7 +14,7 @@ const WorkList = ({ category }: Props) => {
   const works: Work[] = WORKS[category] || [];
 
   return (
-    <div className="grid grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-[22px] mt-[20px] lg:mt-[267px] lg:mb-[206px]">
+    <div className="grid grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-[22px] sm:gap-[14px] mt-[20px] lg:mt-[267px] lg:mb-[206px] sm:w-[90%]">
       {works.map((work, index) => (
         <Link key={index} to={`${PATHS.WORKS}/${category}/${work.id}`}>
           <div
@@ -22,10 +23,10 @@ const WorkList = ({ category }: Props) => {
             key={work.id}
           >
             <div className="flex flex-col items-center">
-              <div
-                className={`w-[288px] h-[216px] 
-								flex justify-center items-center
-								my-[10px] bg-gray-600
+              <img
+                src={gridtest1}
+                className={`w-[288px] h-auto
+								sm:w-full my-[10px]
 								group-hover:border ${
                   category === "COMMUNICATION"
                     ? "border-primary-orange"
@@ -35,18 +36,20 @@ const WorkList = ({ category }: Props) => {
                         ? "border-primary-purple"
                         : "border-primary-blue"
                 }`}
-              >
+                alt={`${work.title}`}
+              />
+              <div className={`flex justify-center items-center`}>
                 <div
-                  className={`hidden w-[201px] h-[24px] group-hover:flex justify-center items-center 
-										${
-                      category === "COMMUNICATION"
-                        ? "bg-primary-orange"
-                        : category === "SERVICE"
-                          ? "bg-primary-red"
-                          : category === "UXUI"
-                            ? "bg-primary-purple"
-                            : "bg-primary-blue"
-                    }`}
+                  className={`hidden lg:w-[201px] lg:h-[24px] lg:group-hover:flex justify-center items-center 
+											${
+                        category === "COMMUNICATION"
+                          ? "bg-primary-orange"
+                          : category === "SERVICE"
+                            ? "bg-primary-red"
+                            : category === "UXUI"
+                              ? "bg-primary-purple"
+                              : "bg-primary-blue"
+                      }`}
                 >
                   <p
                     className={`text-primary-white text-center font-Pretendard_Bold text-[12px]`}
@@ -55,8 +58,9 @@ const WorkList = ({ category }: Props) => {
                   </p>
                 </div>
               </div>
+
               <div
-                className={`w-[288px] mb-[29px] font-TT_Firs_Light text-[15px]`}
+                className={`w-[288px] mb-[29px] sm:w-full sm:mb-[20px] font-TT_Firs_Light text-[15px]`}
               >
                 <p className="text-primary-white">{work.title}</p>
                 <div className="flex">
