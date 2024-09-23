7cf7c3e461978fb5b78b4fd97d71e0921c95954e
minzziPark <minzzi0068@gmail.com>
2024-09-23 19:15:37 +0900

    design : footer 반응형 구현

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,11 +1,12 @@
 const Footer = () => {
   return (
     <div
-      className={`w-full border-t flex justify-center items-center lg:h-[172px]`}
+      className={`w-full border-t flex justify-center items-center py-[35px] sm:py-[20px]
+				text-[20px] md:text-[15px] sm:text-[10px]`}
     >
       <div
-        className={`w-full flex justify-between 
-				text-primary-white text-[20px]
+        className={`w-[90%] lg:w-full flex justify-between 
+				text-primary-white 
 			lg:max-w-lg`}
       >
         <div className={`flex flex-col font-Pretendard_Bold`}>
@@ -14,15 +15,17 @@ const Footer = () => {
           <p>2024 DEGREE SHOW</p>
         </div>
         <div className={`flex flex-col font-Pretendard_Regular`}>
-          <div className={`flex gap-[17px] text-[20px]`}>
+          <div className={`flex gap-[17px]`}>
             <p className={`font-Pretendard_Bold`}>일시</p>
             <p>2024.10.21-2024.10.28</p>
           </div>
-          <div className={`flex gap-[17px] text-[20px]`}>
+          <div className={`flex gap-[17px]`}>
             <p className={`font-Pretendard_Bold`}>장소</p>
             <p>한동대학교 김영길 그레이스 스쿨</p>
           </div>
-          <p className={`text-[12px] text-primary-white/60 mt-[9px]`}>
+          <p
+            className={`text-[12px] md:text-[9px] sm:text-[6px] text-primary-white/60 mt-[6px] lg:mt-[9px]`}
+          >
             ⓒ 2024. Handong Global University CCD. All Rights Reserved.
           </p>
         </div>
