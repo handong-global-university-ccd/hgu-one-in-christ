0a452501d4c8f9daa80b891737da06356f2e5761
minzziPark <minzzi0068@gmail.com>
2024-09-23 22:17:05 +0900

    fix : z-index 설정으로 이미지 삽입 사이드 이펙트 해결

--- src/components/common/PageInfo.tsx
+++ src/components/common/PageInfo.tsx
@@ -6,8 +6,8 @@ const PageInfo = ({ children }: Props) => {
   return (
     <div
       className={`flex justify-center items-center
-		w-screen h-[99px] lg:fixed md:fixed top-16 bg-primary-white 
-		sm:w-[90%] sm:h-[63px] sm:mt-[19px] absolute`}
+		w-screen h-[99px] lg:fixed md:fixed z-50 top-16 bg-primary-white 
+		sm:w-[90%] sm:h-[63px] sm:mt-[19px]`}
     >
       <p
         className={`text-center text-primary-black font-Organetto_ExtBold text-2xl sm:text-[15px]`}
