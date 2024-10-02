9eb87d249085255f3c589c615b3a4cf728989c17
minzziPark <minzzi0068@gmail.com>
2024-10-03 02:16:07 +0900

    design : about 페이지 mainposter 부분 구현

---
+++ src/components/about/MainPoster.tsx
@@ -0,0 +1,13 @@
+import Title from "./Title";
+import mainPoster from "../../assets/about/mainPoster.png"; //test
+
+const MainPoster = () => {
+  return (
+    <div className={`w-full flex flex-col items-center`}>
+      <Title>MAIN POSTER</Title>
+      <img src={mainPoster} className={`w-[500px]`} />
+    </div>
+  );
+};
+
+export default MainPoster;
