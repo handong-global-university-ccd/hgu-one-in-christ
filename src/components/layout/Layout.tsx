39c9e43446489260505c5881735f4c1b29d731df
minzziPark <minzzi0068@gmail.com>
2024-10-10 02:21:05 +0900

    feat : 페이지 이동시 스크롤 위로 이동

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,19 @@
+import { useLocation } from "react-router";
 import Footer from "../common/Footer";
 import Header from "../common/Header";
+import { useEffect } from "react";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <>
       <Header />
