0159bd33f2e42a07ab7dc64d7515ad5d5279005f
minzziPark <minzzi0068@gmail.com>
2024-10-11 05:09:22 +0900

    design : about mainposter 반응형 구현

--- src/components/about/MainPoster.tsx
+++ src/components/about/MainPoster.tsx
@@ -1,11 +1,16 @@
 import Title from "./Title";
-import mainPoster from "../../assets/about/mainPoster.png"; //test
+import { DOMAIN } from "../../constants/paths";
 
 const MainPoster = () => {
   return (
-    <div className={`w-full flex flex-col items-center mb-[219px]`}>
+    <div
+      className={`w-full md:w-[90%] flex flex-col items-center mb-[219px] sm:mb-[144px]`}
+    >
       <Title>MAIN POSTER</Title>
-      <img src={mainPoster} className={`w-[500px]`} />
+      <img
+        src={`${DOMAIN}images/about/mainPoster.webp`}
+        className={`w-[500px] sm:w-full`}
+      />
     </div>
   );
 };
