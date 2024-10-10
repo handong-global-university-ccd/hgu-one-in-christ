4606f85ede65d0c8b56e2a79e2db7d5cb916b3d3
minzziPark <minzzi0068@gmail.com>
2024-10-11 08:34:56 +0900

    design : ux 내용 누락 추가

--- src/components/about/Symbols.tsx
+++ src/components/about/Symbols.tsx
@@ -23,7 +23,7 @@ const symbolInfo = [
     symbol: "(UXUI DESIGN)",
     imgSrc: "images/about/symbol/UXSymbol.webp",
     imgMobSrc: "images/about/symbol/UXSymbolMob.webp",
-    info: "서비스 디자인은 다양한 사용자를 이해하고 여러 이해관계자와 상황을 고려하여 접근해야 한다. 삼각형은 세 변과 세 꼭짓점으로 구성되어 있어 다양한 방식으로 연결되고 변형될 수 있는 특성을 가지고 있다.",
+    info: "UX/UI 디자인에서는 사용자가 제품이나 서비스를 사용하는 과정에서 느끼는 만족감과 편의성을 최대화하는 것이 중요합니다. 원은 중심에서 모든 점이 동일한 거리에 위치하여 균형을 이루고 심리적 안정감을 제공하는 특성으로 UX/UI 디자인의 사용자 경험과 조화를 상징합니다.",
   },
   {
     id: 4,
@@ -94,7 +94,9 @@ const Symbols = () => {
               className={`w-[289px] flex flex-col gap-[10px] text-[16px] text-primary-white hover:text-primary-orange`}
             >
               <p className={`font-medium`}>{item.symbol}</p>
-              <p className={`font-Pretendard_Regular`}>{item.info}</p>
+              <p className={`font-Pretendard_Regular leading-[27px]`}>
+                {item.info}
+              </p>
             </div>
           ))}
         </motion.div>
@@ -143,10 +145,11 @@ const Symbols = () => {
           <div
             className={`font-Pretendard_Regular text-[13px] md:text-[16px] text-primary-purple`}
           >
-            <p>커뮤니케이션 디자인에서는 메시지가 명확하게 전달되야한다.</p>
-            <p>사각형은 형태적으로 단순하고 직관적이며,</p>
-            <p>복잡하지 않아 이해하기 쉽고,</p>
-            <p>구조적으로 안정적이라는 특성을 지니고 있다.</p>
+            <p>UX/UI 디자인에서는 사용자가 제품이나 서비스를 사용하는</p>
+            <p>과정에서 느끼는 만족감과 편의성을 최대화하는 것이</p>
+            <p>중요합니다. 원은 중심에서 모든 점이 동일한 거리에 </p>
+            <p>위치하여 균형을 이루고 심리적 안정감을 제공하는 특성으로</p>
+            <p>UX/UI 디자인의 사용자 경험과 조화를 상징합니다.</p>
           </div>
         </div>
         <div className={`w-full flex flex-col gap-[14px] items-end`}>
@@ -159,7 +162,6 @@ const Symbols = () => {
             className={`font-Pretendard_Regular text-[13px] md:text-[16px] text-primary-blue text-right`}
           >
             <p>제품 디자인은 새로운 아이디어와 기능을 통한</p>
-
             <p>혁신적인 접근이 중요하다. 반원은 한쪽이 직선이고</p>
             <p>다른 한쪽이 곡면으로 전통적인 원의 안정감을 유지하면서</p>
             <p>새로운 접근과 가능성을 탐구할 수 있는 특성을 가지고 있다.</p>
