311afee7455f6dab53fb00af7e8d462773ad2516
minzziPark <minzzi0068@gmail.com>
2024-10-11 08:22:08 +0900

    feat : motion 설정

--- src/components/cursor/CustomCursor.tsx
+++ src/components/cursor/CustomCursor.tsx
@@ -1,4 +1,3 @@
-import styled from "styled-components";
 import { motion } from "framer-motion";
 import { useMousePosition } from "./Context";
 
@@ -7,7 +6,8 @@ const CustomCursor = () => {
 
   return (
     <>
-      <CursorContain
+      <motion.div
+        className={`fixed left-0 top-0 w-[15px] h-[15px] p-[1.5vw] rounded-full bg-white z-20 pointer-events-none opacity-90`}
         animate={hoverNav}
         transition={{ ease: "linear", duration: 0.15 }}
         variants={variants}
@@ -16,17 +16,3 @@ const CustomCursor = () => {
   );
 };
 export default CustomCursor;
-
-const CursorContain = styled(motion.div)`
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 15px;
-  height: 15px;
-  padding: 1.5vw;
-  border-radius: 100%100%;
-  background-color: white;
-  z-index: 20;
-  pointer-events: none;
-  opacity: 0.9;
-`;
