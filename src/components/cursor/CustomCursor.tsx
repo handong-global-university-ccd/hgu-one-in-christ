b88340eb303d95d1fe457bf79515eacbc49ad4ec
minzziPark <minzzi0068@gmail.com>
2024-10-17 03:08:14 +0900

    hotfix : 마우스 커서 모바일 화면 안보이게 해결

--- src/components/cursor/CustomCursor.tsx
+++ src/components/cursor/CustomCursor.tsx
@@ -7,7 +7,7 @@ const CustomCursor = () => {
   return (
     <>
       <motion.div
-        className={`fixed left-0 top-0 w-[15px] h-[15px] p-[1.5vw] rounded-full bg-white z-20 pointer-events-none opacity-90`}
+        className={`fixed left-0 top-0 w-[15px] h-[15px] p-[1.5vw] rounded-full bg-white z-20 pointer-events-none opacity-90 md:hidden sm:hidden`}
         animate={hoverNav}
         transition={{ ease: "linear", duration: 0.15 }}
         variants={variants}
