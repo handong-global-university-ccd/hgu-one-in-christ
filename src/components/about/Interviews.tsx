5cad44e0bf2f12d4bd1970831ec286dc7cecbfed
minzziPark <minzzi0068@gmail.com>
2024-10-20 03:16:15 +0900

    feat : youtube 링크 추가

--- src/components/about/Interviews.tsx
+++ src/components/about/Interviews.tsx
@@ -4,21 +4,22 @@ import Title from "./Title";
 const Interviews = () => {
   return (
     <div
-      className={`w-full lg:max-w-lg flex flex-col items-center mb-[300px] mb-[170px]`}
+      className={`w-full lg:max-w-lg flex flex-col items-center mb-[300px] sm:mb-[170px]`}
     >
       <Title>INTERVIEWS</Title>
       <div className={`w-[90%] relative pb-[56.25%] mb-[22px]`}>
         <YouTube
           className={`absolute w-full h-full`}
-          videoId="2Id5kY0-PwE" //동영상 주소
+          videoId="9KZscZuMj4k" //동영상 주소
           opts={{
             width: "100%",
             height: "100%",
             playerVars: {
-              autoplay: 0,
+              autoplay: 1,
               modestbranding: 0, //컨트롤 바에 유튜브 로고 표시 여부
-              loop: 0, //반복 재생
-              playlist: "2Id5kY0-PwE", //반복 재생으로 재생할 플레이 리스트
+              mute: 1,
+              loop: 1, //반복 재생
+              playlist: "9KZscZuMj4k", //반복 재생으로 재생할 플레이 리스트
             },
           }}
         />
@@ -31,9 +32,10 @@ const Interviews = () => {
             width: "100%",
             height: "100%",
             playerVars: {
-              autoplay: 0,
+              autoplay: 1,
               modestbranding: 0,
-              loop: 0,
+              mute: 1,
+              loop: 1,
               playlist: "E8oM6SBUrcI",
             },
           }}
