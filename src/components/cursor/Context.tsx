6865777fb7881e7311222edc1d5fa5aca60ec30d
minzziPark <minzzi0068@gmail.com>
2024-10-19 19:26:26 +0900

    ui : 마우스 속도 빠르게

--- src/components/cursor/Context.tsx
+++ src/components/cursor/Context.tsx
@@ -21,7 +21,7 @@ export const MouseContextProvider = ({ children }: Props) => {
       zIndex: 1000,
       transition: {
         type: "tween",
-        duration: 0.05,
+        duration: 0,
         ease: "linear",
       },
     },
@@ -37,7 +37,7 @@ export const MouseContextProvider = ({ children }: Props) => {
       zIndex: 1000,
       transition: {
         type: "tween",
-        duration: 0.1,
+        duration: 0,
         ease: "linear",
       },
     },
@@ -53,7 +53,7 @@ export const MouseContextProvider = ({ children }: Props) => {
       zIndex: 1000,
       transition: {
         type: "tween",
-        duration: 0.05,
+        duration: 0,
         ease: "linear",
       },
     },
@@ -67,7 +67,7 @@ export const MouseContextProvider = ({ children }: Props) => {
       zIndex: 1000,
       transition: {
         type: "tween",
-        duration: 0.05,
+        duration: 0,
         ease: "linear",
       },
     },
@@ -83,7 +83,7 @@ export const MouseContextProvider = ({ children }: Props) => {
       zIndex: 1000,
       transition: {
         type: "tween",
-        duration: 0.05,
+        duration: 0,
         ease: "linear",
       },
     },
