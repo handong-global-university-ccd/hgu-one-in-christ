ea3965e55c56fc727b6f2e44e2bd3795e96c5c6c
minzziPark <minzzi0068@gmail.com>
2024-10-19 19:05:57 +0900

    ui : work scrollTop

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -1,11 +1,15 @@
 import WorksCategoryButton from "../components/works/WorksCategoryButton";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import WorkList from "../components/works/WorkList";
 import { WorkCategory } from "../models/category.model";
 import PageInfo from "../components/common/PageInfo";
 
 const Works = () => {
   const [category, setCategory] = useState<WorkCategory>("COMMUNICATION");
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [category]);
 
   return (
     <div className="flex flex-col items-center">
