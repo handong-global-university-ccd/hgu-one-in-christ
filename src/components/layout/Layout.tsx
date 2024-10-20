7cb1e4d1e409fc9492ee8be1f8b8eefb7abff1f7
minzziPark <minzzi0068@gmail.com>
2024-10-21 00:08:26 +0900

    feat : 스크롤 버튼 생성

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,23 +1,72 @@
 import { useLocation } from "react-router";
 import Footer from "../common/Footer";
 import Header from "../common/Header";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
+import { DOMAIN } from "../../constants/paths";
+import { useMousePosition } from "../cursor/Context";
+import { motion } from "framer-motion";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const { smallEnter, defaultEnter } = useMousePosition();
+
   const location = useLocation();
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleScroll = () => {
+    const mainElement = ref.current;
+    if (!mainElement) return;
+
+    const mainTop = mainElement.getBoundingClientRect().top;
+
+    if (mainTop < -200) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const handleUp = () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location]);
 
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
       <Header />
-      <main className="w-full">{children}</main>
+      <main ref={ref} className="w-full">
+        {children}
+        <motion.img
+          onMouseEnter={smallEnter}
+          onMouseLeave={defaultEnter}
+          src={`${DOMAIN}images/icon/scrollButton.webp`}
+          alt="up-button"
+          className={`${
+            isVisible ? "fixed" : "hidden"
+          } w-50 lg:bottom-[30%] lg:right-150 bottom-[5%] right-[5%]`}
+          onClick={handleUp}
+        />
+      </main>
       <Footer />
     </>
   );
