3ea2874b35a6175291aeed8b21343202bc3a3aa6
minzziPark <minzzi0068@gmail.com>
2024-10-24 14:40:27 +0900

    design : title 마우스 커서 조정

--- src/components/about/Title.tsx
+++ src/components/about/Title.tsx
@@ -1,14 +1,21 @@
+import { useMousePosition } from "../cursor/Context";
+import { motion } from "framer-motion";
+
 interface Props {
   children: React.ReactNode;
 }
 
 const Title = ({ children }: Props) => {
+  const { smallEnter, defaultEnter } = useMousePosition();
+
   return (
-    <div
+    <motion.div
+      onMouseEnter={smallEnter}
+      onMouseLeave={defaultEnter}
       className={`w-full font-Organetto_ExtBold text-25 sm:text-15 text-primary-white leading-[120px] text-center mb-50`}
     >
       {children}
-    </div>
+    </motion.div>
   );
 };
 
